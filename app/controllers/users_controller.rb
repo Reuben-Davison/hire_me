55e9b58c288309048ad4d012aa6375c68d916168
reuben <creubend@gmail.com>
2022-09-23 09:12:21 -0600

    feat: Users error handling working as expected for invalid update, boiler plate test still not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,36 +19,22 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users or /users.json
+  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save 
-      format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+      redirect_to user_url(@user), notice: "User was successfully created." 
     else  
       render :new, status: 200 
     end
-
-    # respond_to do |format|
-    #   if @user.save
-    #     format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-    #     format.json { render :show, status: :created, location: @user }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  # PATCH/PUT /users/1
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to user_url(@user), notice: "User was successfully updated." 
+    else
+      redirect_to user_url(@user), notice:"User could not be updated"
     end
   end
 

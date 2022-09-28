70da84b16403ca9e44721d20a8ae5d0f4b9d37f5
reuben <creubend@gmail.com>
2022-09-28 08:44:16 -0600

    Clean up comments and changed wording for notice.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,18 +44,16 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url, notice: "User was successfully deleted." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :email)
     end
